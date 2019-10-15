7bdc9ccb190f00a411afcdc2cbf5012aeb1d169a
kailbin <ykb553@163.com>
2019-10-15 15:09:05 +0800

    异步执行

--- demo-spring-context/demo-spring-context-event/src/main/java/xyz/kail/demo/spring/context/event/Main.java
+++ demo-spring-context/demo-spring-context-event/src/main/java/xyz/kail/demo/spring/context/event/Main.java
@@ -3,18 +3,24 @@ package xyz.kail.demo.spring.context.event;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.event.TransactionalEventListener;
+import xyz.kail.demo.spring.context.event.config.AppConfig;
 import xyz.kail.demo.spring.context.event.event.ConditionEvent;
 import xyz.kail.demo.spring.context.event.event.DemoEvent;
 import xyz.kail.demo.spring.context.event.event.StatusEvent;
 
+import javax.annotation.Resource;
 import java.util.Date;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
+@Configuration
 @ComponentScan(basePackageClasses = Main.class)
 public class Main {
 
-    public static void main(String[] args) {
-        final ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+    public static void main(String[] args) throws InterruptedException {
+        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
 
         // 发布事件
         context.publishEvent(new DemoEvent("demo:" + new Date()));
@@ -29,6 +35,8 @@ public class Main {
 
         // 针对事务的事件处理
         final Class<TransactionalEventListener> transactionalEventListenerClass = TransactionalEventListener.class;
+
+        TimeUnit.SECONDS.sleep(10);
 
     }
 
